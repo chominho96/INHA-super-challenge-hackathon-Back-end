25707a0de49f19a584c017da7ab88c480b7b1d8d
CHO Min Ho <chominho96@inha.edu>
2022-01-18 17:36:38 +0900

    #6 : Add Covid19 Info class
    1. add Covid19 Info class (by open api)
    2. add Covid19 Info Controller class
    3. edit Member class

---
+++ demo/src/main/java/HitecIsFuture/demo/web/covid19_info/Covid19Info.java
@@ -0,0 +1,23 @@
+package HitecIsFuture.demo.web.covid19_info;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Covid19Info {
+    // 기준 일자
+    private Date date;
+
+    // 확진자 수
+    private int confirmedCase;
+
+    // 사망자 수
+    private int deadPeople;
+
+    public Covid19Info(Date date, int confirmedCase, int deadPeople) {
+        this.date = date;
+        this.confirmedCase = confirmedCase;
+        this.deadPeople = deadPeople;
+    }
+}
--- demo/src/main/java/HitecIsFuture/demo/web/member/Member.java
+++ demo/src/main/java/HitecIsFuture/demo/web/member/Member.java
@@ -2,12 +2,16 @@ package HitecIsFuture.demo.web.member;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
-import java.util.Date;
 
 @Data
+@Entity
 public class Member {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
