6ef834025d0d78c0e6a9d252f2b5ecafb0b60b89
CHO Min Ho <chominho96@inha.edu>
2022-01-18 17:03:26 +0900

    #2 : Edit Member serivce
    1. edit member service

--- demo/src/main/java/HitecIsFuture/demo/domain/service/MemberService.java
+++ demo/src/main/java/HitecIsFuture/demo/domain/service/MemberService.java
@@ -1,5 +1,6 @@
 package HitecIsFuture.demo.domain.service;
 
+
 import HitecIsFuture.demo.web.member.Member;
 import HitecIsFuture.demo.web.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +13,16 @@ public class MemberService {
 
     // 회원가입
     public Long join(Member member) {
-        memberRepository.findBy
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
 
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw  new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 
 
